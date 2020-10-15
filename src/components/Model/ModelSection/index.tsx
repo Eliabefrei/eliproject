8f4e085c0a604757d5de524a928d5c696ccd2910
Eliabefrei <santana.eliabe93@gmail.com>
2020-10-15 10:38:57 -0300

    added landind page

---
+++ src/components/Model/ModelSection/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ModelSection: React.FC = () => {
+  return (
+    <Container>
+      <h1>ModelSection</h1>
+    </Container>
+  );
+};
+
+export default ModelSection;
